e4ed49d68c19cea58181347a733bade418f53252
Grantland Hall <grantlandhall@berkeley.edu>
2016-04-03 08:12:11 -0700

    Why

---
+++ bad.sh
@@ -0,0 +1,5 @@
+# What's the traffic to my app?
+zgrep -h "/myapp" /var/log/archive/apache.* | gawk --re-interval -v ipre='[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}' 'match($0,ipre,ip) && !(ip[0] in ips) {ips[ip[0]]; print}' | sort -k1M -k2n | uniq -cw6 | awk '{print $2" "$3": "$1}'
+
+# Convert a list of IPs for an IP->Email map.
+cat outdated.tsv|gawk 'BEGIN {f="t";q="select i.ip, r.email from r join i on r.cid = i.cid where i.active=QtQ and i.ip in ("}/ipaddr=/{match($0,/ipaddr=([0-9.]+)/,ip);if(f=="t"){f="f"}else{q=q","}q=q"Q"ip[1]"Q"}END{q=q");";print q}'|tr Q \'|psql -h dbhost db|head -n-2|tail -n+3|sed 's/|/,/g'|tee emails.csv
