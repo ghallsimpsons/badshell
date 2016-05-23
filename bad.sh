355ac6e419e5b7a4876f920a8f736ede727ace7c
Grantland Hall <grantlandhall@berkeley.edu>
2016-05-23 13:46:35 -0700

    That one coworker

--- bad.sh
+++ bad.sh
@@ -3,3 +3,11 @@ zgrep -h "/myapp" /var/log/archive/apache.* | gawk --re-interval -v ipre='[0-9]{
 
 # Convert a list of IPs for an IP->Email map.
 cat outdated.tsv|gawk 'BEGIN {f="t";q="select i.ip, r.email from r join i on r.id = i.id where i.active=QtQ and i.ip in ("}/ipaddr=/{match($0,/ipaddr=([0-9.]+)/,ip);if(f=="t"){f="f"}else{q=q","}q=q"Q"ip[1]"Q"}END{q=q");";print q}'|tr Q \'|psql -h dbhost db|head -n-2|tail -n+3|sed 's/|/,/g'|tee emails.csv
+
+# Handle that misbehaving co-worker.
+git(){
+	if [ "$1" = "add" ]; then
+		find $(git rev-parse --show-toplevel) -name \*.py -exec sed -i.bak 's/\t/    /g' {} +;
+	fi
+	/usr/bin/git $@
+}
